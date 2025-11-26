5844b97952dbfef2fdb687cb6aeec9497394a9f3
pankaj1132 <sainipankaj1102@gmail.com>
2025-11-26 16:20:43 +0530

    add

--- src/app/in/[slug]/page.jsx
+++ src/app/in/[slug]/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ContactForm from '@/components/ContactForm';
+import ContactForm from '../../../Components/ContactForm';
 // Dynamic SEO metadata for each location page
 export function generateMetadata({ params }) {
  const { slug } = params;
